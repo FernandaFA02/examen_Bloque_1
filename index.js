74dedcd95d378a7fe57912f5b8abb2a9aa4f325d
FernandaFA02 <fajardo.al9402@gmail.com>
2021-11-11 11:46:27 -0600

    modificando

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 fetch('./data.json')
 .then((response)=> response.json())
 .then((data)=> bootcampData(data))
+.catch((error) => console.log(error))
 
 let bootcampData = (data) => {
     for (const item of data.Bootcamp_integrantes) {
@@ -9,7 +10,6 @@ let bootcampData = (data) => {
         console.log(item.edad)
         console.log(item.cumpleaños)
         let miembros = document.getElementById('BootcampMembers')
-        let listaC = document.getElementById('lista')
         miembros.innerHTML += `<h2>${item.nombre} ${item.apellido}</h2>
         <ul><li>${item.cumpleaños}</li></ul>`
         
